8d7a427f7373aff1a0a51fb173f28174fbd5f72f
matthewdeancooper <matthewdeancooper@gmail.com>
2020-11-13 16:46:54 +1100

    updated

--- config.h
+++ config.h
@@ -37,16 +37,16 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-                                 { "[t]",      tile },    /* first entry is default */
-                                 /* { "[d]",      deck },    /\* first entry is default *\/ */
+                                 { "[t]",      tile },  
+                                 { "[d]",      deck },   
                                  { "[m]",      monocle },
-                                 { "[f]",      NULL },    /* no layout function means floating behaviour */
+                                 { "[f]",      NULL },   
 };
 
 /* key definitions */
@@ -123,8 +123,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_d,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_d,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[3]} },
 	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
 	{ MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
